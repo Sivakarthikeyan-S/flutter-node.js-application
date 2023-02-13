3146de42ddcb09f657af73bd5ee0d72bbceda946
sivakarthi6055@gmail.com <sivakarthi6055@gmail.com>
2023-02-13 16:17:24 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:amazon_node/constants/global_variables.dart';
+import 'package:amazon_node/features/auth/screens/auth_screen.dart';
+
+import 'package:amazon_node/router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
