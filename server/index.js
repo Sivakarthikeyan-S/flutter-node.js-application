2bc53306380bf603864069258645c021be5b3ced
sivakarthi6055@gmail.com <sivakarthi6055@gmail.com>
2023-02-14 18:16:12 +0530

    Added User Model

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
-console.log("Hello, World")
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+
+// INIT
+const PORT = 3000;
+const app = express();
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+// middleware
+app.use(authRouter);
+
+// Connections
+mongoose.set('strictQuery', false);
+
+mongoose.connect(DB).then(() => {
+        console.log('Connection Successful');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`);
+});
